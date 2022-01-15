8af310dd13725a840f37a1f0262c4a751e64cc87
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-01-15 19:11:10 +0100

    feat: read csv using pandas and geocode coordinates using geopy

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+import pandas
+from flask import Flask, render_template, request, send_file
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("home.html")
+
+
+@app.route("/success", methods=["POST"])
+def success():
+    if request.method == "POST":
+        file = request.files["file"]
+        df = pandas.read_csv(file)
+        gc = Nominatim()
+        df["coordinates"] = df["Address"].appy(gc.geocode)
+        df["Latitude"] = df["coordinates"].appy(
+            lambda x: x.latitude if x != None else None
+        )
+        df["Longitude"] = df["coordinates"].appy(
+            lambda x: x.longitude if x != None else None
+        )
+        df = df.drop("coordinates", 1)
+
+        df.to_csv("uploads/geocoded.csv", index=None)
+
+        return render_template(
+            "home.html", datatable=df.to_html(), btn="download.html"
+        )
+
+
+@app.route("/download")
+def download():
+    return send_file(
+        filename, attachment_filename="super-geocoder.csv", as_attachment=True
+    )
 
 
 if __name__ == "__main__":
